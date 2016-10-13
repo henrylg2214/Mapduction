32656d4423cb98e1faf52a6f21554a17cae15a82
henrylg2214 <henry@admins-mbp.vfs.local>
2016-10-12 19:33:10 -0700

    added rectangle with kapow

--- script.js
+++ script.js
@@ -3,15 +3,37 @@
  */
 var mapsapi = 'AIzaSyBgbI_nNqjw1HM7PUPOw_0a37Cy5sVEuaE'
 var map;
+var bounds = {
+    north: 49.282,
+    south: 49.281,
+    east: -123.114,
+    west: -123.115
+}
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 49.283, lng: -123.111},
         zoom: 16
     });
 
-google.maps.event.addListener(map,'click',function (event) {
-    console.log(event.latLng.lat())
-    console.log(event.latLng.lng())
-})
+    google.maps.event.addListener(map, 'click', function (event) {
+        console.log(event.latLng.lat())
+        console.log(event.latLng.lng())
+    })
+    rectangle = new google.maps.Rectangle({
+        bounds: bounds,
+        editable: true,
+        draggable: true
+    });
+
+    rectangle.setMap(map);
+
+    google.maps.event.addListener(rectangle, 'click', function (event) {
+alert ('kapow')
+    })
+
+
+
+
 
 }
